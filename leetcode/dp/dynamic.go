3ec84443f6e12bafc02b7d2721dd366ad50b4d61
王瑞 <wangrui5@songguo7.com>
2022-06-22 16:24:00 +0800

    maxprofit

--- leetcode/dp/dynamic.go
+++ leetcode/dp/dynamic.go
@@ -1,5 +1,7 @@
 package dp
 
+import "math"
+
 // https://leetcode.cn/leetbook/read/top-interview-questions-easy/xn854d/
 // 爬楼梯
 func climbStairs(n int) int {
@@ -8,8 +10,31 @@ func climbStairs(n int) int {
 	for i := 1; i < n; i++ {
 		p = q
 		q = r
-		r = p+q
+		r = p + q
 	}
 
 	return r
+}
+
+// https://leetcode.cn/leetbook/read/top-interview-questions-easy/xn8fsh/
+// 买卖股票的最佳时机
+func maxProfit(prices []int) int {
+	if len(prices) == 1 {
+		return 0
+	}
+
+	minPrice := math.MaxInt64
+	maxProfit := 0
+
+	for i := 0; i < len(prices); i++ {
+		if prices[i] < minPrice {
+			minPrice = prices[i]
+		}
+
+		if prices[i] > minPrice && prices[i] - minPrice > maxProfit {
+			maxProfit = prices[i] - minPrice
+		}
+	}
+
+	return maxProfit
 }
